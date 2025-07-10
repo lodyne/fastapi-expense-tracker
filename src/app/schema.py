89b6d2e1a2c7c26100230f2196e49188f814cd26
lodyne <lodgmtui@gmail.com>
2025-07-10 14:16:07 +0300

    feat(schema):add new pydantic schemas

--- src/app/schema.py
+++ src/app/schema.py
@@ -3,6 +3,7 @@
 This module defines input validation schemas for expenses.
 """
 
+from beanie import PydanticObjectId
 from pydantic import BaseModel, Field
 
 
@@ -18,4 +19,43 @@ class ExpenseIn(BaseModel):
 
     name: str = Field(..., description="Name of the expense")
     amount: float = Field(..., description="Amount of the expense")
-    category: str = Field(..., description="Category of the expense")
+    category: PydanticObjectId = Field(..., description="Category of the expense")
+    budget: PydanticObjectId | None = Field(
+        None, description="Budget associated with the expense"
+    )
+
+class CategoryIn(BaseModel):
+    """
+    Schema for creating a new category.
+
+    Attributes:
+        name (str): Name of the category.
+    """
+
+    name: str = Field(..., description="Name of the category")
+
+class BudgetIn(BaseModel):
+    """
+    Schema for creating a new budget.
+
+    Attributes:
+        name (str): Name of the budget.
+        amount (float): Total amount allocated for the budget.
+    """
+
+    name: str = Field(..., description="Name of the budget")
+    amount: float = Field(..., description="Total amount allocated for the budget")
+
+class CategoryOut(BaseModel):
+
+    name: str
+
+class BudgetOut(BaseModel):
+    name: str
+    amount: float
+
+class ExpenseOut(BaseModel):
+    name: str
+    amount: float
+    category: CategoryOut
+    budget: BudgetOut
