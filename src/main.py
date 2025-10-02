70686383f27615ad09525ebf1921f8ac183b5511
lodyne <lodgmtui@gmail.com>
2025-10-02 08:56:52 +0300

    feat: modify main function

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ lifespan for database initialization, and includes the API routes.
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.app.database.mongo import init_db
-from src.app.routes.mongo import router as mongo_router
-# from src.app.routes.postgres import router as postgres_router
+# from src.app.routes.mongo import router as mongo_router
+from src.app.routes.postgres import router as postgres_router
 
 from src.app.utils import cors_config
 
@@ -34,7 +34,7 @@ app = FastAPI(
         {"name": "categories", "description": "Operations related to categories"},
         {"name": "budgets", "description": "Operations related to budgets"},
     ],
-    lifespan=lifespan,
+    # lifespan=lifespan,
     docs_url="/docs",
     redoc_url="/redoc",
     openapi_url="/openapi.json",
@@ -56,5 +56,5 @@ async def root():
     return {"message": "Hello, World!"}
 
 
-app.include_router(mongo_router)
-# app.include_router(postgres_router)
+# app.include_router(mongo_router)
+app.include_router(postgres_router)
